9fca907905a4545a81dd4dfe2f2eff3f946b6acb
Felipe Guimarães <felipeguimaraes2406@gmail.com>
2021-04-29 13:29:33 -0300

    add 201 status to request

--- src/main/java/com/usuario/cadUsuario/resources/UserResource.java
+++ src/main/java/com/usuario/cadUsuario/resources/UserResource.java
@@ -1,5 +1,7 @@
 package com.usuario.cadUsuario.resources;
 
+import java.net.URI;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,6 +10,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.usuario.cadUsuario.entities.User;
 import com.usuario.cadUsuario.services.UserService;
@@ -28,6 +31,8 @@ public class UserResource {
 	@PostMapping
 	public ResponseEntity<User> insert(@RequestBody User obj) {
 		obj = service.insert(obj);
-		return ResponseEntity.ok().body(obj);
+		
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).body(obj);
 	}
 }
